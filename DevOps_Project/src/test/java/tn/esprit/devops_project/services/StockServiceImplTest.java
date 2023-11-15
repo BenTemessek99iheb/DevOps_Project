7ce436031814828398cd1673d834a2dcd1eba962
Ben Temessek iheb <iheb.bentemessek@esprit.tn>
2023-11-15 02:52:49 +0100

    Unit Test for Stock
    +adding junit and mockito in pom.xml

--- DevOps_Project/src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
+++ DevOps_Project/src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.devops_project.entities.Stock;
 import tn.esprit.devops_project.repositories.StockRepository;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -32,9 +34,38 @@ class StockServiceImplTest {
 
     @Test
     void retrieveStock() {
+        // Given
+        Stock stock = new Stock();
+        stock.setTitle("Test Stock");
+        Stock savedStock = stockRepository.save(stock);
+        Long id = savedStock.getIdStock();
+
+        // When
+        Stock retrievedStock = stockRepository.findById(id).orElse(null);
+
+        // Then
+        assertNotNull(retrievedStock);
+        assertEquals(id, retrievedStock.getIdStock());
+        assertEquals("Test Stock", retrievedStock.getTitle());
     }
 
     @Test
     void retrieveAllStock() {
+        // Given
+        Stock stock1 = new Stock();
+        stock1.setTitle("Stock 1");
+        Stock stock2 = new Stock();
+        stock2.setTitle("Stock 2");
+        stockRepository.save(stock1);
+        stockRepository.save(stock2);
+
+        // When
+        List<Stock> stockList = stockRepository.findAll();
+
+        // Then
+        assertTrue(stockList.size() >= 2);
+        assertTrue(stockList.stream().anyMatch(stock -> "Stock 1".equals(stock.getTitle())));
+        assertTrue(stockList.stream().anyMatch(stock -> "Stock 2".equals(stock.getTitle())));
+
     }
 }
